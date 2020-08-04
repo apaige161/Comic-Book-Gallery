ed0957cd13e58d4c3a70df02740b05f3aab00624
Alex Paige <apaige161@gmail.com>
2020-08-04 18:43:46 -0400

    added comic books controller

---
+++ Comic-Book-Gallery/Controllers/ComicBookController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Comic_Book_Gallery.Controllers
+{
+    //needs to reference the base controllers class
+    public class ComicBookController : Controller
+    {
+    }
+}
