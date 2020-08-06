d6b53fceb4b91d7397b5456ee4d5f2b72370b475
Alex Paige <apaige161@gmail.com>
2020-08-05 21:17:45 -0400

    updated models

---
+++ Comic-Book-Gallery/Models/Artist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Comic_Book_Gallery.Models
+{
+    public class Artist
+    {
+        //since the artist object contains a key value pair we set up artists in its own class
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+    }
+}
---
+++ Comic-Book-Gallery/Models/ComicBook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Comic_Book_Gallery.Models
+{
+    public class ComicBook
+    {
+        //set property values
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+
+        //set as an array of artist pbject that has it own attributes
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            //readonly
+            get
+            {
+                return SeriesTitle + "#" + IssueNumber;
+            }
+        }
+
+        //attach a comic cover with comic book
+        public string CoverImageFileNme
+        {
+            get 
+            {
+                //replace any spaces with -
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber
+                    + ".jpg";
+            }
+        }
+
+    }
+}
